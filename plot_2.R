a2918b5bf0dba960b0f59ccd525ba5450ba449f2
Gerald Anderson <gander@loki.org>
2015-05-08 14:56:25 -0500

    tweaked code formatting

--- plot_2.R
+++ plot_2.R
@@ -4,6 +4,7 @@ plot_2_filename = "plot_2.png"
 
 png(filename=plot_2_filename, width=480, height=480)
 
-plot(data$Date_time, data$Global_active_power, type="line", xlab="", ylab="Global Active Power (kilowatts)")
+plot(data$Date_time, data$Global_active_power, type="l", 
+     xlab="", ylab="Global Active Power (kilowatts)")
 
 dev.off()
